6ef2adb3109e19c41300fe63b672d639e755da70
Stefan Mark <mark@unserver.de>
2015-03-04 11:13:27 +0100

    srandr

--- scripts/suspend.sh
+++ scripts/suspend.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 function susprep {
+    which nvidia-disable.sh &> /dev/null && nvidia-disable.sh
     for A in $(ls /media/); do pumount $A; done;
+    killall ssh
     netcfg -a
     sync
 }
 
 function suspres {
+    killall -s SIGHUP srandrd
     echo resume
 }
 
